6f3bc32203344ec0dee56e5f769a970e689f437f
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-12-13 12:07:58 +0100

    Refactor mechanic to select scan area

--- app/src/main/java/com/example/simplescanner/BoxImageView.java
+++ app/src/main/java/com/example/simplescanner/BoxImageView.java
@@ -12,7 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class BoxImageView extends androidx.appcompat.widget.AppCompatImageView {
-    private Paint paint;
+    private Paint paintLine;
     private Point topLeft;
     private Point topRight;
     private Point bottomLeft;
@@ -20,6 +20,7 @@ public class BoxImageView extends androidx.appcompat.widget.AppCompatImageView {
     private List<Point> points;
     private int canvasWidth;
     private int canvasHeight;
+    private int closestPoint;
     boolean boxIsInitialized;
 
     private static final double DISTANCE_TO_MOVE_EDGE = 100;
@@ -33,13 +34,13 @@ public class BoxImageView extends androidx.appcompat.widget.AppCompatImageView {
     }
 
     private void initMembers() {
-        paint = new Paint();
-        paint.setDither(true);
-        paint.setColor(0xFFFF0000);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(5);
+        paintLine = new Paint();
+        paintLine.setDither(true);
+        paintLine.setColor(0xFFFF0000);
+        paintLine.setStyle(Paint.Style.STROKE);
+        paintLine.setStrokeJoin(Paint.Join.ROUND);
+        paintLine.setStrokeCap(Paint.Cap.ROUND);
+        paintLine.setStrokeWidth(5);
 
         topLeft = new Point();
         topRight = new Point();
@@ -84,10 +85,33 @@ public class BoxImageView extends androidx.appcompat.widget.AppCompatImageView {
             bottomRight.set(canvasWidth - drawOffset, canvasHeight - drawOffset);
             boxIsInitialized = true;
         }
-        canvas.drawLine(topLeft.x, topLeft.y, topRight.x, topRight.y, paint);
-        canvas.drawLine(topRight.x, topRight.y, bottomRight.x, bottomRight.y, paint);
-        canvas.drawLine(bottomRight.x, bottomRight.y, bottomLeft.x, bottomLeft.y, paint);
-        canvas.drawLine(bottomLeft.x, bottomLeft.y, topLeft.x, topLeft.y, paint);
+        canvas.drawLine(topLeft.x, topLeft.y, topRight.x, topRight.y, paintLine);
+        canvas.drawLine(topRight.x, topRight.y, bottomRight.x, bottomRight.y, paintLine);
+        canvas.drawLine(bottomRight.x, bottomRight.y, bottomLeft.x, bottomLeft.y, paintLine);
+        canvas.drawLine(bottomLeft.x, bottomLeft.y, topLeft.x, topLeft.y, paintLine);
+    }
+
+    private void updateClosestPoint(double x, double y) {
+        if (x < 0) x = 0;
+        else if (x > canvasWidth) x = canvasWidth;
+        if (y < 0) y = 0;
+        else if (y > canvasHeight) y = canvasHeight;
+        points.get(closestPoint).set((int) x, (int) y);
+        invalidate();
+    }
+
+    private void findClosestPoint(double x, double y) {
+        double minDistance = Double.MAX_VALUE;
+        for (int j = 0; j < 4; j++) {
+            Point p = points.get(j);
+            double dx = x - p.x;
+            double dy = y - p.y;
+            double distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPoint = j;
+            }
+        }
     }
 
     @Override
@@ -95,25 +119,16 @@ public class BoxImageView extends androidx.appcompat.widget.AppCompatImageView {
         if (!boxIsInitialized) {
             return true;
         }
-        if (event.getAction() == MotionEvent.ACTION_MOVE || event.getAction() == MotionEvent.ACTION_DOWN) {
-            float x = event.getX();
-            float y = event.getY();
-            Log.d("BoxImageView", "Touch event at " + x + ", " + y);
-            for (int j = 0; j < 4; j++) {
-                Point p = points.get(j);
-                double dx = x - p.x;
-                double dy = y - p.y;
-                double distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < DISTANCE_TO_MOVE_EDGE) {
-                    if (x < 0) x = 0;
-                    else if (x > canvasWidth) x = canvasWidth;
-                    if (y < 0) y = 0;
-                    else if (y > canvasHeight) y = canvasHeight;
-                    points.get(j).set((int) x, (int) y);
-                    invalidate();
-                    return true;
-                }
-            }
+        int action = event.getAction();
+        float x = event.getX();
+        float y = event.getY();
+        if (action == MotionEvent.ACTION_DOWN) {
+            findClosestPoint(x, y);
+            updateClosestPoint(x, y);
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            updateClosestPoint(x, y);
+        } else if (action == MotionEvent.ACTION_UP) {
+            closestPoint = -1;
         }
         return true;
     }
