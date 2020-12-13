a5bd11deec443c5e95e8cd5a5ab92b23341f8d06
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-12-13 12:10:50 +0100

    Remove unused code in EditActivity

--- app/src/main/java/com/example/simplescanner/EditActivity.java
+++ app/src/main/java/com/example/simplescanner/EditActivity.java
@@ -11,8 +11,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,27 +20,6 @@ public class EditActivity extends AppCompatActivity {
     private Bitmap editedImage;
 
     @Override
-    protected void onDestroy() {
-        // TODO: Using onDestory does not seem to work to finalize an intent!
-        super.onDestroy();
-        Intent intent = new Intent();
-        intent.putExtra("photoPath", currentPhotoPath);
-        try {
-            File file = new File(currentPhotoPath);
-            file.delete();
-            file.createNewFile();
-            FileOutputStream fileOut = new FileOutputStream(file);
-            editedImage.compress(Bitmap.CompressFormat.JPEG, 90, fileOut);
-            fileOut.flush();
-            fileOut.close();
-            setResult(RESULT_OK, intent);
-        } catch (IOException e) {
-            Log.d(getClass().getSimpleName(), "Failed to save edited image: " + e.getMessage());
-            setResult(RESULT_CANCELED, intent);
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
@@ -50,7 +27,7 @@ public class EditActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         currentPhotoPath = intent.getStringExtra("photoPath");
-        Log.d("EditActivity", "Start activity to edit photo " + currentPhotoPath);
+        Log.d(getClass().getSimpleName(), "Start activity to edit photo " + currentPhotoPath);
 
         BoxImageView previewView = findViewById(R.id.previewView);
         editedImage = Utils.getImage(currentPhotoPath);
