27b61d7aede0f7c71c951e7849554e01152cad36
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-12-14 10:50:39 +0100

    Add text to about page and put beta version number

--- app/src/main/java/com/example/simplescanner/AboutActivity.java
+++ app/src/main/java/com/example/simplescanner/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.example.simplescanner;
 
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,38 @@ public class AboutActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        TextView textAbout = findViewById(R.id.textAbout);
+        textAbout.setLineSpacing(0, 1.2f);
+        String text = "";
+
+        text += "<b>Simple Scanner</b> Android application<br/>";
+        text += "<b>Website</b> github.com/stwunsch/android-simplescanner<br/>";
+        text += "<b>License</b> GPL v3.0<br/>";
+
+        text += "<br/><b>Dependencies</b><br/>";
+
+        text += "<br/>";
+        text += "<b>BoofCV</b> Image processing library<br/>";
+        text += "<b>Website</b> boofcv.org<br/>";
+        text += "<b>License</b> Apache v2.0<br/>";
+
+        text += "<br/>";
+        text += "<b>PDFBox Android</b> PDF processing library<br/>";
+        text += "<b>Website</b> github.com/TomRoush/PdfBox-Android/<br/>";
+        text += "<b>License</b> Apache v2.0<br/>";
+
+        textAbout.setText(Html.fromHtml(text));
+
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+            String version = pInfo.versionName;
+
+            TextView textVersion = findViewById(R.id.textVersion);
+            textVersion.setText("Version " + version);
+        } catch (Exception e) {
+            Log.d(getClass().getSimpleName(), "Failed to get version name: " + e.getMessage());
         }
     }
 
